0aaae6bbb35b51cb4159b3cc4f11eaa0998fb16e
cohenhenoudanoam-cmd <henoudanoam@gmail.com>
2026-03-23 19:26:15 +0100

    Add files via upload

--- copie clee usb(tool).py
+++ copie clee usb(tool).py
@@ -1,2 +1,17 @@
 #obj: creer une copie d'une clef usb donc recupere les fichiers d'une clef usb et les copie dans un dossier de destination
 import os
+import subprocess
+import psutil
+import pyusb
+import time
+
+def detecter_cle_usb():
+    while True:
+        time.sleep(5)
+        cmd_pour_check= subprocess.run("Get-WmiObject -Class Win32_LogicalDisk", shell=True)
+        if cmd_pour_check.returncode != 0:
+            print("Clé USB détectée !")
+            print(cmd_pour_check)
+        else:
+            print("Aucune clé USB détectée.")
+        pass
